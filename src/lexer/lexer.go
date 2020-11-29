eb0d3fba47e57fac6d5df930c157f228b0cdc0b7
jimao <1982775886@qq.com>
2020-11-29 23:59:04 +0800

    chunkNext

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -34,9 +34,43 @@ func (me *LuaLexer) CurColumn() int {
 	return me.curColumn
 }
 
+func isWhiteSpace(c byte) bool {
+	switch c {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
+	}
+	return false
+}
+
+// skipComment 跳过注释
+func (me *LuaLexer) skipComment() {
+
+}
+
 // skipInvalidCodes 跳过无效代码
 func (me *LuaLexer) skipInvalidCodes() {
 	fmt.Println("跳过无效代码")
+	for len(me.chunk) > 0 {
+		if me.chunkStartsWith("--") {
+			me.skipComment()
+		} else if me.chunkStartsWith("\n\r") {
+			me.chunkNextN(2)
+			me.chunkNewLine()
+		} else if me.chunkStartsWith("\r\n") {
+			me.chunkNextN(2)
+			me.chunkNewLine()
+		} else if me.chunkStartsWith("\n") {
+			me.chunkNext()
+			me.chunkNewLine()
+		} else if me.chunkStartsWith("\r") {
+			me.chunkNext()
+			me.chunkNewLine()
+		} else if me.chunkTopIsWhiteSpace() {
+			me.chunkNext()
+		} else {
+			break
+		}
+	}
 }
 
 // chunkNewLine 新一行
@@ -45,9 +79,37 @@ func (me *LuaLexer) chunkNewLine() {
 	me.curColumn = 1
 }
 
+// chunkNewLines 新的多行
+func (me *LuaLexer) chunkNewLines(n int) {
+	me.curLine += n
+	me.curColumn = 1
+}
+
+// chunkNext 下一个字符
+func (me *LuaLexer) chunkNext() {
+	me.chunk = me.chunk[1:]
+	me.curColumn++
+}
+
+// chunkNext 下n个字符
+func (me *LuaLexer) chunkNextN(n int) {
+	for i := 0; i < n; i++ {
+		me.chunkNext()
+	}
+}
+
 // chunkStartsWith chunk是否以特定文本开始
 func (me *LuaLexer) chunkStartsWith(text string) bool {
 	return strings.HasPrefix(me.chunk, text)
+}
+
+func (me *LuaLexer) chunkTopIsWhiteSpace() bool {
+	c := me.chunk[0]
+	switch c {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
+	}
+	return false
 }
 
 // NextToken 解析下一个单词

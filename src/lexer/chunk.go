f10eea4b0199dcef99b3e3e63b20b2879db05e45
jimao <1982775886@qq.com>
2020-12-02 15:59:49 +0800

    chunk大致写好

--- src/lexer/chunk.go
+++ src/lexer/chunk.go
@@ -1,32 +1,71 @@
 package lexer
 
-// Chunk 词法分析器待处理的Chunk
-type Chunk string
+// Chunk Chunk类型
+type Chunk struct {
+	text      string
+	curLine   int
+	curColumn int
+}
+
+// Text 当前待解析的文本
+func (me *Chunk) Text() string {
+	return me.text
+}
+
+// CurLine 当前行
+func (me *Chunk) CurLine() int {
+	return me.curLine
+}
+
+// CurColumn 当前列
+func (me *Chunk) CurColumn() int {
+	return me.curColumn
+}
+
+// Top Chunk头部
+func (me *Chunk) Top() ChunkTop {
+	return ChunkTop(me.text)
+}
 
 // IsEmpty Chunk是否为空
-func (me Chunk) IsEmpty() bool {
-	return len(me) < 1
+func (me *Chunk) IsEmpty() bool {
+	return len(me.text) < 1
 }
 
-// Top 获取Chunk头部
-func (me Chunk) Top() ChunkTop {
-	return ChunkTop(me)
+// Next 下移处理一个字符
+func (me *Chunk) Next() string {
+	top := me.Top().Char()
+	me.text = me.text[1:]
+	return top
 }
 
-// Next s
-func (me Chunk) Next() string {
-	return ""
+// NextN 下移处理n个字符
+func (me *Chunk) NextN(n int) string {
+	rst := ""
+	for i := 0; i < n; i++ {
+		rst += me.Next()
+	}
+	return rst
 }
 
-// NextN s
-func (me Chunk) NextN(n int) string {
-	return ""
+// NewLine 新的一行
+func (me *Chunk) NewLine() {
+	me.curLine++
+	me.curColumn = 1
 }
 
-func (me Chunk) NewLine() {
-
+// NewLineN 新的n行
+func (me *Chunk) NewLineN(n int) {
+	for i := 0; i < n; i++ {
+		me.NewLine()
+	}
 }
 
-func (me Chunk) NewLineN(n int) {
-
+// NewChunk 构造函数
+func NewChunk(text string) *Chunk {
+	return &Chunk{
+		text:      text,
+		curLine:   1,
+		curColumn: 1,
+	}
 }

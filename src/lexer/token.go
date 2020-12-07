06777e406ef26ae6559be8991ffb585519a8c3b5
jimao <1982775886@qq.com>
2020-12-07 14:28:13 +0800

    token.go review完成

--- src/lexer/token.go
+++ src/lexer/token.go
@@ -32,29 +32,56 @@ func (me *LuaToken) LineEnd() int {
 	return me.lineEnd
 }
 
-// CharStart 单词列开始
+// Line 单词行范围
+func (me *LuaToken) Line() (int, int) {
+	return me.LineStart(), me.LineEnd()
+}
+
+// CharStart 单词字符开始
 func (me *LuaToken) CharStart() int {
 	return me.charStart
 }
 
-// CharEnd 单词列结束
+// CharEnd 单词字符结束
 func (me *LuaToken) CharEnd() int {
 	return me.charEnd
 }
 
-// Line 当前行（行开始）
-func (me *LuaToken) Line() int {
-	return me.LineStart()
+// Char 单词字符范围
+func (me *LuaToken) Char() (int, int) {
+	return me.CharStart(), me.CharEnd()
 }
 
-// Char 当前列（列开始）
-func (me *LuaToken) Char() int {
-	return me.CharStart()
+// SetLineStart 设置单词所在行开始行号
+func (me *LuaToken) SetLineStart(value int) {
+	me.lineStart = value
 }
 
-// Print 打印Token信息
-func (me *LuaToken) Print() {
-	fmt.Printf("%s\t%s\n", me.Text(), me.TokenType().Name())
+// SetLineEnd 设置单词所在行结束行号
+func (me *LuaToken) SetLineEnd(value int) {
+	me.lineEnd = value
+}
+
+// SetLine 设置单词所在的行范围
+func (me *LuaToken) SetLine(start, end int) {
+	me.SetLineStart(start)
+	me.SetLineEnd(end)
+}
+
+// SetCharStart 设置单词所在的字符开始位置
+func (me *LuaToken) SetCharStart(value int) {
+	me.charStart = value
+}
+
+// SetCharEnd 设置单词所在的字符结束位置
+func (me *LuaToken) SetCharEnd(value int) {
+	me.charEnd = value
+}
+
+// SetChar 设置单词所在的字符范围
+func (me LuaToken) SetChar(start, end int) {
+	me.SetCharStart(start)
+	me.SetCharEnd(end)
 }
 
 // NewLuaToken 构造函数
@@ -66,4 +93,19 @@ func NewLuaToken(
 		tokenType: tokenType,
 		text:      text,
 	}
+}
+
+// Print 打印Token信息（用于调试）
+func (me *LuaToken) Print() {
+	line1, line2 := me.Line()
+	char1, char2 := me.Char()
+	fmt.Printf(
+		"%s\t%s\t%d %d\t%d %d\n",
+		me.Text(),
+		me.TokenType().Name(),
+		line1,
+		line2,
+		char1,
+		char2,
+	)
 }
